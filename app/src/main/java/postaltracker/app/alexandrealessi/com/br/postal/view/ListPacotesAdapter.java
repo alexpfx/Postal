a0f7d204043f63f0c64dca8dcd307682369f1ba6
alexpfx <alexpfba@gmail.com>
2015-03-08 23:02:37 -0300

    layouts

--- app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/ListPacotesAdapter.java
+++ app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/ListPacotesAdapter.java
@@ -20,9 +20,9 @@ import postaltracker.app.alexandrealessi.com.br.postal.R;
 /**
  * Created by alexandre on 06/03/15.
  */
-public class ListPacotesAdapter  extends RecyclerView.Adapter<ListPacotesAdapter.ViewHolder> {
+public class ListPacotesAdapter extends RecyclerView.Adapter<ListPacotesAdapter.ViewHolder> {
 
-    private DateFormat dateFormat = SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
+    private DateFormat dateFormat = SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT, Locale.getDefault());
     private List<Item> items;
 
 
@@ -30,8 +30,8 @@ public class ListPacotesAdapter  extends RecyclerView.Adapter<ListPacotesAdapter
         this.items = items;
     }
 
-    public interface ItemClickListener{
-        public void itemClicked (Item item);
+    public interface ItemClickListener {
+        public void itemClicked(Item item);
     }
 
     @Override
@@ -44,13 +44,26 @@ public class ListPacotesAdapter  extends RecyclerView.Adapter<ListPacotesAdapter
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Item item = items.get(position);
-        holder.setSro(item.sro);
+        holder.txtSro.setText(item.sro);
+                holder.txtDataAcao.setText(dateFormat.format(item.dataAcao).concat(": "));
+        holder.txtAcao.setText(item.acao);
+        StringBuilder sb = new StringBuilder();
+        List<String> tags = item.tags;
+        if (tags != null){
+            for (String tag:tags){
+                sb.append(tag).append(" ");
+            }
+        }
+        holder.txtTags.setText(sb.toString());
+
         holder.btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "teste", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 
     @Override
@@ -58,20 +71,22 @@ public class ListPacotesAdapter  extends RecyclerView.Adapter<ListPacotesAdapter
         return items.size();
     }
 
-    public static final class Item {
+    static final class Item {
+        private final Date dataAcao;
+        private final List<String> tags;
         private String acao, sro;
-        private boolean atualizado;
-
-        public Item(String acao, String sro) {
+        public Item(String acao, Date dataAcao, String sro, List<String> tags) {
             this.acao = acao;
             this.sro = sro;
+            this.dataAcao = dataAcao;
+            this.tags = tags;
         }
-
     }
 
-     static class ViewHolder extends RecyclerView.ViewHolder{
-        private TextView txtSro;
-        private ImageButton btnLocation;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView txtSro, txtDataAcao, txtAcao, txtTags;
+        private final ImageButton btnLocation, btnExpand, btnDirections;
+
         private ItemClickListener itemClickListener;
 
         public void setItemClickListener(ItemClickListener itemClickListener) {
@@ -79,32 +94,31 @@ public class ListPacotesAdapter  extends RecyclerView.Adapter<ListPacotesAdapter
         }
 
 
-        private  ViewHolder(View itemView, TextView txtSro, ImageButton btnLocation) {
+        private ViewHolder(View itemView, TextView txtSro, TextView txtAcao, TextView txtDataAcao, TextView txtTags, ImageButton btnLocation, ImageButton btnDirections, ImageButton btnExpand) {
             super(itemView);
             this.txtSro = txtSro;
             this.btnLocation = btnLocation;
+            this.txtAcao = txtAcao;
+            this.txtDataAcao = txtDataAcao;
+            this.btnDirections = btnDirections;
+            this.btnExpand = btnExpand;
+            this.txtTags = txtTags;
+
         }
 
-        private static ViewHolder newInstance (View v){
-            TextView txtAcao, txtSro;
+        private static ViewHolder newInstance(View v) {
+            TextView txtAcao, txtSro, txtDataAcao, txtTags;
             txtSro = (TextView) v.findViewById(R.id.txtSro);
-            ImageButton btnLocationButton = (ImageButton) v.findViewById(R.id.btnLocation);
-           return new ViewHolder(v, txtSro, btnLocationButton);
+            ImageButton btnLocation = (ImageButton) v.findViewById(R.id.btnExpand);
+            ImageButton btnExpand = (ImageButton) v.findViewById(R.id.btnExpand);
+            ImageButton btnDirections = (ImageButton) v.findViewById(R.id.btnExpand);
+            txtAcao = (TextView) v.findViewById(R.id.txtAcao);
+            txtDataAcao = (TextView) v.findViewById(R.id.txtDataAcao);
+            txtTags = (TextView) v.findViewById(R.id.txtTags);
+            return new ViewHolder(v, txtSro, txtAcao, txtDataAcao, txtTags, btnLocation, btnDirections, btnExpand);
         }
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
-
-        public void setSro(String txtSro) {
-            this.txtSro.setText(txtSro);
-        }
-
-        public void setBtnLocation(ImageButton btnLocation) {
-            this.btnLocation = btnLocation;
-        }
     }
-
 
 
 }
--- app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/ListaPacotesFragment.java
+++ app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/ListaPacotesFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import postaltracker.app.alexandrealessi.com.br.postal.R;
 
@@ -43,17 +44,188 @@ public class ListaPacotesFragment extends Fragment {
         recyclerView.setLayoutManager(lm);
         adapter = new ListPacotesAdapter(createFakeItems());
         recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),  null));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),  null));
 
         return v;
     }
 
+    private Random rand = new Random();
     private List<ListPacotesAdapter.Item> createFakeItems() {
         return new ArrayList<ListPacotesAdapter.Item>(){
             {
-                add(new ListPacotesAdapter.Item("detalhe", "DM 123456789 BR"));
-                add(new ListPacotesAdapter.Item("palhoca", "XP 446797914 CN"));
-                add(new ListPacotesAdapter.Item("floripa", "DR 149740790 BR"));
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("destinatário não apresentou-se para receber", new Date (rand.nextLong()),   "DM 123456789 BR", new ArrayList<String>(){
+                    {
+                        add("Submarino");
+                        add("laptop");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("entregue", new Date (rand.nextLong()), "DP 123646449 AR", new ArrayList<String>(){
+                    {
+                        add("xxxxxxxxxx");
+                        add("xxxxxxxxx");
+                        add("xxxxxxxxx");
+                    }
+                }));
+
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("destinatário não apresentou-se para receber", new Date (rand.nextLong()),   "DM 123456789 BR", new ArrayList<String>(){
+                    {
+                        add("Submarino");
+                        add("laptop");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("entregue", new Date (rand.nextLong()), "DP 123646449 AR", new ArrayList<String>(){
+                    {
+                        add("xxxxxxxxxx");
+                        add("xxxxxxxxx");
+                        add("xxxxxxxxx");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("destinatário não apresentou-se para receber", new Date (rand.nextLong()),   "DM 123456789 BR", new ArrayList<String>(){
+                    {
+                        add("Submarino");
+                        add("laptop");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("entregue", new Date (rand.nextLong()), "DP 123646449 AR", new ArrayList<String>(){
+                    {
+                        add("xxxxxxxxxx");
+                        add("xxxxxxxxx");
+                        add("xxxxxxxxx");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("destinatário não apresentou-se para receber", new Date (rand.nextLong()),   "DM 123456789 BR", new ArrayList<String>(){
+                    {
+                        add("Submarino");
+                        add("laptop");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("entregue", new Date (rand.nextLong()), "DP 123646449 AR", new ArrayList<String>(){
+                    {
+                        add("xxxxxxxxxx");
+                        add("xxxxxxxxx");
+                        add("xxxxxxxxx");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("Entrega não efetuada por motivos operacionais", new Date (rand.nextLong()), "DM 543496494 BR", new ArrayList<String>(){
+                    {
+                        add("Siciliano");
+                        add("Livro");
+                        add("Presente");
+                    }
+                }));
+
+
+                add(new ListPacotesAdapter.Item("destinatário não apresentou-se para receber", new Date (rand.nextLong()),   "DM 123456789 BR", new ArrayList<String>(){
+                    {
+                        add("Submarino");
+                        add("laptop");
+                    }
+                }));
+                add(new ListPacotesAdapter.Item("entregue", new Date (rand.nextLong()), "DP 123646449 AR", new ArrayList<String>(){
+                    {
+                        add("xxxxxxxxxx");
+                        add("xxxxxxxxx");
+                        add("xxxxxxxxx");
+                    }
+                }));
+
             }
         };
 
