e0ffa1087baafde6c88b2e57a2547646722e187f
alexpfx <alexpfba@gmail.com>
2015-03-23 17:11:46 -0300

    adicionado itens na actionbar para o fragmento cadastro pacotes.

--- app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/CadastroPacotesFragment.java
+++ app/src/main/java/postaltracker/app/alexandrealessi/com/br/postal/view/CadastroPacotesFragment.java
@@ -1,12 +1,18 @@
 package postaltracker.app.alexandrealessi.com.br.postal.view;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import postaltracker.app.alexandrealessi.com.br.postal.R;
 
 /**
@@ -22,8 +28,16 @@ public class CadastroPacotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cadastro_pacotes, container, false);
+        setHasOptionsMenu(true);
+        View view = inflater.inflate(R.layout.fragment_cadastro_pacotes, container, false);
+        ButterKnife.inject(this, view);
+        return view;
+
     }
 
-
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_cadastro_pacotes, menu);
+    }
 }
