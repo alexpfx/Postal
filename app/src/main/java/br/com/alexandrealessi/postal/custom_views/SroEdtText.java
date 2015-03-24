9181a1efea4d90f79f15419c42140b279e60413b
alexpfx <alexpfba@gmail.com>
2015-03-24 20:51:56 -0300

    criado SroEdtTextWatcher para componente o customizado SroEdtText

--- app/src/main/java/br/com/alexandrealessi/postal/custom_views/SroEdtText.java
+++ app/src/main/java/br/com/alexandrealessi/postal/custom_views/SroEdtText.java
@@ -4,12 +4,16 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.alexandrealessi.postal.R;
 
@@ -19,23 +23,49 @@ import br.com.alexandrealessi.postal.R;
  */
 public class SroEdtText extends LinearLayout {
 
-    EditText edtNumber;
 
-    AutoCompleteTextView edtServiceType;
+    private final List<SroEdtTextWatcher> textChangeListeners = new ArrayList<>();
 
-    AutoCompleteTextView edtCountry;
+    private EditText edtNumber;
+
+    private AutoCompleteTextView edtServiceType;
+
+    private AutoCompleteTextView edtCountry;
 
     public SroEdtText(Context context) {
         super(context);
         init(context);
+
     }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            for (SroEdtTextWatcher tw : textChangeListeners) {
+                tw.sroChanged(s, getSro());
+            }
+        }
+    };
 
     private void init(Context context) {
         final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.custom_sro_edttext, this);
         edtNumber = (EditText) view.findViewById(R.id.edtSroNumber);
+        edtNumber.addTextChangedListener(textWatcher);
         edtServiceType = (AutoCompleteTextView) view.findViewById(R.id.edtSroServiceType);
+        edtServiceType.addTextChangedListener(textWatcher);
         edtCountry = (AutoCompleteTextView) view.findViewById(R.id.edtSroCountry);
+        edtCountry.addTextChangedListener(textWatcher);
+
     }
 
     public SroEdtText(Context context, AttributeSet attrs) {
@@ -70,4 +100,17 @@ public class SroEdtText extends LinearLayout {
     public Editable getNumber() {
         return edtNumber.getText();
     }
+
+    public interface SroEdtTextWatcher {
+        public void sroChanged(Editable whichComponent, String newSro);
+    }
+
+    public void addTextChangelistener (SroEdtTextWatcher textWatcher){
+        textChangeListeners.add(textWatcher);
+    }
+    public void removeTextChangeListener (SroEdtTextWatcher textWatcher){
+        textChangeListeners.remove(textWatcher);
+    }
+
+
 }
--- app/src/main/java/br/com/alexandrealessi/postal/view/CadastroPacotesFragment.java
+++ app/src/main/java/br/com/alexandrealessi/postal/view/CadastroPacotesFragment.java
@@ -3,15 +3,19 @@ package br.com.alexandrealessi.postal.view;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import br.com.alexandrealessi.postal.R;
+import br.com.alexandrealessi.postal.custom_views.SroEdtText;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +25,9 @@ public class CadastroPacotesFragment extends Fragment {
     private MenuItem actionPaste;
     private MenuItem actionAccept;
     private MenuItem actionCancel;
+
+    @InjectView(R.id.edtSro)
+    SroEdtText edtSro;
 
     public CadastroPacotesFragment() {
     }
@@ -32,6 +39,13 @@ public class CadastroPacotesFragment extends Fragment {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_cadastro_pacotes, container, false);
         ButterKnife.inject(this, view);
+        edtSro
+                .addTextChangelistener(new SroEdtText.SroEdtTextWatcher() {
+                    @Override
+                    public void sroChanged(Editable whichComponent, String newSro) {
+                        Toast.makeText(getActivity().getApplicationContext(), newSro, Toast.LENGTH_SHORT ).show();
+                    }
+                });
         return view;
 
     }
@@ -40,8 +54,8 @@ public class CadastroPacotesFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         actionPaste = menu.findItem(R.id.action_paste);
-        actionAccept= menu.findItem(R.id.action_accept);
-        actionCancel= menu.findItem(R.id.action_cancel);
+        actionAccept = menu.findItem(R.id.action_accept);
+        actionCancel = menu.findItem(R.id.action_cancel);
         inflater.inflate(R.menu.menu_cadastro_pacotes, menu);
     }
 
