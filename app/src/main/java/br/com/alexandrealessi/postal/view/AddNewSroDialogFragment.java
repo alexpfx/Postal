e2887e58287b0c012a37d90e5cac8d23871659da
alexpfx <alexpfba@gmail.com>
2015-04-04 00:20:27 -0300

    adicionado onClick vazio no dialog.

--- app/src/main/java/br/com/alexandrealessi/postal/view/AddNewSroDialogFragment.java
+++ app/src/main/java/br/com/alexandrealessi/postal/view/AddNewSroDialogFragment.java
@@ -3,7 +3,9 @@ package br.com.alexandrealessi.postal.view;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import br.com.alexandrealessi.postal.R;
 
@@ -13,12 +15,21 @@ import br.com.alexandrealessi.postal.R;
 public class AddNewSroDialogFragment extends DialogFragment {
 
 
+    private String tag = AddNewSroDialogFragment.class.getName();
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.fragment_cadastro_pacotes, null)).setPositiveButton(R.string.accept, null).setNegativeButton(R.string.cancel, null);
-
+        builder.setView(inflater.inflate(R.layout.fragment_cadastro_pacotes, null)).setPositiveButton(R.string.accept, acceptClick).setNegativeButton(R.string.cancel, null);
         return builder.create();
     }
+    public DialogInterface.OnClickListener acceptClick = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            Log.d(tag, "onAcceptClick");
+        }
+    };
+
 }
