8f868a85457bdb300581c556e49f3f6e6174950c
alexpfx <alexpfba@gmail.com>
2015-04-10 00:32:03 -0300

    esqueletos EventoDao

---
+++ app/src/main/java/br/com/alexandrealessi/postal/model/database/dao/EventoDao.java
@@ -0,0 +1,15 @@
+package br.com.alexandrealessi.postal.model.database.dao;
+
+import br.com.alexandrealessi.postal.model.domain.Evento;
+
+import java.util.List;
+
+/**
+ * Created by alexandre on 10/04/15.
+ */
+public interface EventoDao {
+
+    Evento insert(Evento evento);
+
+    List<Evento> getAll();
+}
---
+++ app/src/main/java/br/com/alexandrealessi/postal/model/database/dao/EventoDaoImpl.java
@@ -0,0 +1,20 @@
+package br.com.alexandrealessi.postal.model.database.dao;
+
+import br.com.alexandrealessi.postal.model.domain.Evento;
+
+import java.util.List;
+
+/**
+ * Created by alexandre on 10/04/15.
+ */
+public class EventoDaoImpl implements EventoDao {
+    @Override
+    public Evento insert(Evento evento) {
+        return null;
+    }
+
+    @Override
+    public List<Evento> getAll() {
+        return null;
+    }
+}
