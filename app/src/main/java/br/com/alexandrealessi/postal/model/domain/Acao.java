93de2feca5dcd4334ce1fd6f84209212e19772a7
alexpfx <alexpfba@gmail.com>
2015-04-11 11:10:14 -0300

    atualizacao dominio Acao.

--- app/src/main/java/br/com/alexandrealessi/postal/model/domain/Acao.java
+++ app/src/main/java/br/com/alexandrealessi/postal/model/domain/Acao.java
@@ -3,17 +3,29 @@ package br.com.alexandrealessi.postal.model.domain;
 /**
  * Created by alexandre on 09/03/15.
  */
-public class Acao {
+public class Acao extends BaseDomain {
     private String descricao;
 
-    public Acao() {
+
+    private Acao(Long id, String descricao) {
+        super(id);
+        this.descricao = descricao;
+    }
+
+    private Acao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public static final Acao create(Long id, String descricao) {
+        return new Acao(id, descricao);
+    }
+
+    public static final Acao create(String descricao) {
+        return new Acao(descricao);
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
